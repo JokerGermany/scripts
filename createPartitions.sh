11fc11936badccc63a67ea6f45105b60fd4d159f
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2024-10-20 17:29:39 +0200

    Create createPartitions.sh

---
+++ createPartitions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+diskGB="$3"
+sliceGB=1000
+partsize=$(($sliceGB*1000*1000*1000/1024/1024 - 1))
+
+for ((i=1; i <= $(($diskGB/$sliceGB)); i++))
+do
+    sudo parted -a optimal -- "$1" mkpart md"$i"-"$2" $(($i+($i-1)*$partsize))MiB $(($i+$i*$partsize))MiB
+done
+sudo parted "$1" print free
